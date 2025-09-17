d42e8c8a1d1ca109e91337dbe9299f93805418c8
muhammadali-imran <imranmuhammadali633@gmail.com>
2025-09-17 16:29:09 +0500

    Created the todo_items model

--- django_todo/todo_app/models.py
+++ django_todo/todo_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class todo_items(models.Model):
+    title = models.CharField(max_length=200)
+    done = models.BooleanField(default=False)
