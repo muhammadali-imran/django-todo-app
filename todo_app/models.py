a2e458e4de2e2076ba79a4a150dbd2ca1267800f
muhammadali-imran <imranmuhammadali633@gmail.com>
2026-08-16 10:31:58 +0500

    Updated Todo Model

--- todo_app/models.py
+++ todo_app/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 # Create your models here.
 class todo_items(models.Model):
-    title = models.CharField(max_length=200)
-    done = models.BooleanField(default=False)
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    status = models.BooleanField(default=False)
+    timeCreated = models.DateField(auto_now_add=True)
+    timeUpdated = models.DateField(auto_now=True)
